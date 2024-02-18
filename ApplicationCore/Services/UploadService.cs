1100b791c1ebe3978eaa24ebd4e97cce3c911033
kiespetch <kiespetch@ya.ru>
2024-02-18 19:11:35 +0300

    добавлена параллельность при обработке задач

--- ApplicationCore/Services/UploadService.cs
+++ ApplicationCore/Services/UploadService.cs
@@ -39,7 +39,6 @@ public class UploadService : IUploadService
     /// <inheritdoc cref="IUploadService.UploadNew()"/>
     public async Task<List<Issue>> UploadNew()
     {
-        //GlobalVariables.LastDbUpdateTime = new DateTime(2024, 2, 13,15,0,0);
         var address = $"issues?query=updated: {GlobalVariables.LastDbUpdateTime:yyyy-MM-dd} .. *";
         using var response = await _client.GetClient.GetAsync(address);
         var jsonResponse = await response.Content.ReadAsStringAsync();
@@ -60,17 +59,18 @@ public class UploadService : IUploadService
         const string workLogQueryUrl = "/timeTracking/workItems?fields=author(login),creator(login)," +
                                        "duration(id,minutes),text,date";
 
-        foreach (var entry in idList)
+        var tasks = idList.Select(async entry =>
         {
             using var issueResponse = await _client.GetClient.GetAsync("issues/" + entry.Id + issueQueryUrl);
-            var tamp = "https://testjiraintegration.youtrack.cloud/api/" + entry.Id + workLogQueryUrl;
             var jsonIssueData = await issueResponse.Content.ReadAsStringAsync();
 
             using var workLogResponse = await _client.GetClient.GetAsync("issues/" + entry.Id + workLogQueryUrl);
             var jsonWorkLogData = await workLogResponse.Content.ReadAsStringAsync();
 
-            if (jsonIssueData == null) continue;
+            if (jsonIssueData == null) return;
+
             customFields = JsonConvert.DeserializeObject<List<CustomFieldInfo>>(jsonIssueData, settings);
+
             if (jsonWorkLogData != "[]")
             {
                 var workLog = JsonConvert.DeserializeObject<List<WorkLogInfo>>(jsonWorkLogData, settings);
@@ -82,7 +82,9 @@ public class UploadService : IUploadService
                 issues.Add(JsonConvert.DeserializeObject<Issue>(jsonIssueData, settings)
                     .AddCustomParameters(customFields));
             }
-        }
+        });
+
+        await Task.WhenAll(tasks);
         
         return issues;
     }
